f7acf77348276339b78d81b1a19914942e446731
mimifasol <mimifaso@gmail.com>
2023-07-29 10:58:48 +0800

    feat: add user module

--- todo-list-express-master/models/seeds/todoSeeder.js
+++ todo-list-express-master/models/seeds/todoSeeder.js
@@ -1,3 +1,4 @@
+/*
 const Todo = require('../todo')
 const db = require('../../config/mongoose')
 
@@ -8,3 +9,31 @@ db.once('open', () => {
 
   console.log('done')
 })
+*/
+
+
+const mongoose = require('mongoose')
+const Todo = require('../todo') // 載入 todo model
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.once('open', () => {
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: 'name-' + i })
+  }
+
+  console.log('done')
+})
+
+/*
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+*/
