abf3c365cd6888774f0736ce7f209742a27b3a2b
mimifasol <mimifaso@gmail.com>
2023-07-29 13:28:02 +0800

    feat: add register page

--- todo-list-express-master/routes/modules/users.js
+++ todo-list-express-master/routes/modules/users.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-router.post('/login', (req, res) => {
+router.get('/login', (req, res) => {
+  res.render('login')
 })
+
+router.post('/login', (req, res) => {
+
+})
+
 router.get('/register',(req, res) => {
   res.render('register')
 })
