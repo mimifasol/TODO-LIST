5c1426a709377d42672183cb084299bd793d284b
mimifasol <mimifaso@gmail.com>
2023-07-31 15:26:44 +0800

    feat: add user registration feature

--- todo-list-express-master/models/user.js
+++ todo-list-express-master/models/user.js
@@ -9,11 +9,11 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: true
-  }
+  },
   password: {
     type: String,
     required: true
-  }
+  },
   createdAt: {
     type: Date,
     default: Date.now
--- todo-list-express-master/routes/modules/users.js
+++ todo-list-express-master/routes/modules/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user') /*引入user model */
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -11,6 +13,31 @@ router.post('/login', (req, res) => {
 
 router.get('/register',(req, res) => {
   res.render('register')
+})
+
+router.post('/register',(req, res)=>{
+  //取得註冊表單參數
+  const { name, email, password, confirmPassword } = req.body
+  //檢查使用者是否已註冊
+  User.findOne({ email }).then(user => {
+    if (user){
+      console.log('User already exists.')
+      res.render('register',{//再附上表單參數
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }else {
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(()=> res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+  }).catch(err => console.log(err))
 })
 
 module.exports = router
