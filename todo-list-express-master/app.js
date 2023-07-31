14c428824841cd67b935de42c09c2791ba2ddf03
mimifasol <mimifaso@gmail.com>
2023-08-01 01:12:01 +0800

    feat:add express-session

--- todo-list-express-master/app.js
+++ todo-list-express-master/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session') //使用者登入功能之第三方套件
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -15,6 +16,13 @@ const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
