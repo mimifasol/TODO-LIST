2321d0d219b72e448bd56d3614dacba195177e24
mimifasol <mimifaso@gmail.com>
2023-08-02 20:29:22 +0800

    feat: add message partials

--- todo-list-express-master/app.js
+++ todo-list-express-master/app.js
@@ -3,6 +3,7 @@ const session = require('express-session') //使用者登入功能之第三方�
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const usePassport = require('./config/passport')//載入passport設定檔
 
 if (process.env.NODE_ENV !== 'production') {
@@ -28,10 +29,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
