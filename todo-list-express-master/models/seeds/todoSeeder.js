a45686d812d0b7ebb1efd4c47382f14d49491597
mimifasol <mimifaso@gmail.com>
2023-08-05 12:20:28 +0800

    modify todoSeeder

--- todo-list-express-master/models/seeds/todoSeeder.js
+++ todo-list-express-master/models/seeds/todoSeeder.js
@@ -1,3 +1,37 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const Todo = require('../todo')
+const User = require('../user')
+const db = require('../../config/mongoose')
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
+db.once('open', () => {
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 10 },
+        (_, i) => Todo.create({ name: `name-${i}`, userId })
+      ))
+    })
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
+})
+
 /*
 const Todo = require('../todo')
 const db = require('../../config/mongoose')
@@ -11,7 +45,7 @@ db.once('open', () => {
 })
 */
 
-
+/*
 const mongoose = require('mongoose')
 const Todo = require('../todo') // 載入 todo model
 if (process.env.NODE_ENV !== 'production') {
